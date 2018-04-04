b5375af3ad2c56a325c6483f3e413c4db19e0e43
David Broadfoot <dbroadfoot@gmail.com>
2018-04-04 19:23:58 +1000

    Ci (#1)
    * set up travis ci
    
    * finalize CI

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('LICENSE') as f:
 
 setup(
     name='pygogogate2',
-    version='0.0.3',
+    version='0.0.5',
     description='Python package for controlling Gogogate2-Enabled Garage Door',
     author='David Broadfoot',
     author_email='dbroadfoot@gmail.com',
