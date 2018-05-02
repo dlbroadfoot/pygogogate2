af096aa76ec2cc2a7f71e6e1a3193dac9359e4d2
David Broadfoot <dbroadfoot@gmail.com>
2018-05-03 07:38:02 +1000

    Resolved conflicts

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('LICENSE') as f:
 
 setup(
     name='pygogogate2',
-    version='0.0.7',
+    version='0.0.8',
     description='Python package for controlling Gogogate2-Enabled Garage Door',
     author='David Broadfoot',
     author_email='dbroadfoot@gmail.com',
